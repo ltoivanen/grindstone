2a4032ad7d2635dde8bf05ef5b58c087adcbf033
ltoivanen <leevi.toivanen@hotmail.fi>
2024-03-17 18:24:01 +0200

    Updated image paths

--- src/components/footer.js
+++ src/components/footer.js
@@ -25,10 +25,10 @@ function Footer() {
             </div>
 
             <div class="footer-oikea">
-                <a href="https://www.lenovo.com/fi/fi/" target="_blank"><img class="logot" src="./grindstone/kumppanit/IDG_Authorized_200x150.webp"/></a>
-                <a href="https://www.lenovo.com/fi/fi/" target="_blank"><img class="logot" src="./grindstone/kumppanit/ISG_Authorized_200x150.webp"/></a>
-                <a href="https://www.cisco.com/" target="_blank"><img class="logot" src="./grindstone/kumppanit/cisco-partner-logo-select-provider-300x274.webp"/></a>
-                <a href="https://www.withsecure.com/fi/home" target="_blank"><img class="logot" src="./grindstone/kumppanit/withsecure_logo_charcoal_black.png"/></a>
+                <a href="https://www.lenovo.com/fi/fi/" target="_blank"><img class="logot" src="../grindstone/kumppanit/IDG_Authorized_200x150.webp"/></a>
+                <a href="https://www.lenovo.com/fi/fi/" target="_blank"><img class="logot" src="../grindstone/kumppanit/ISG_Authorized_200x150.webp"/></a>
+                <a href="https://www.cisco.com/" target="_blank"><img class="logot" src="../grindstone/kumppanit/cisco-partner-logo-select-provider-300x274.webp"/></a>
+                <a href="https://www.withsecure.com/fi/home" target="_blank"><img class="logot" src="../grindstone/kumppanit/withsecure_logo_charcoal_black.png"/></a>
             </div>
         </div>
     );
